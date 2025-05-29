187552dd5533203e0a289fc30b57ca2e02e2f02c
Karan_d6v1n9 <132968913+D6v1n9@users.noreply.github.com>
2025-05-29 18:27:11 +0000

    Created custom react app

---
+++ 03 customReact/customreact.js
@@ -0,0 +1,33 @@
+// This file will render the element into index.html file similar to index.js in react
+
+function customRender(reactElement, container) {
+    /*
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.props.href)
+    domElement.setAttribute('target', reactElement.props.target)
+
+    container.appendChild(domElement)
+    */
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props) {
+        if (prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+    container.appendChild(domElement)
+    
+}
+ // the react element is parsed (Internally react also do the same for the element)
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click to visit google'
+};
+
+const mainContainer = document.getElementById('root');
+customRender(reactElement, mainContainer);
