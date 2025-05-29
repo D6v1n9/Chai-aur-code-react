e0a55e20c65530731778fe618aa729bd3dadbd87
Karan_d6v1n9 <132968913+D6v1n9@users.noreply.github.com>
2025-05-29 18:00:43 +0000

    Update vite in 02 understandFlow

--- 02 Understand Flow/01_vite_react/src/main.jsx
+++ 02 Understand Flow/01_vite_react/src/main.jsx
@@ -1,8 +1,16 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
