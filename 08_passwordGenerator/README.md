599e44c6eb2085b934d3f5d23a6506c5dbe9078c
D6v1n9 <karansinghlodhi6194u@gmail.com>
2025-06-01 13:39:48 +0530

    PasswordGenerator project

--- 08_passwordGenerator/README.md
+++ 08_passwordGenerator/README.md
@@ -1,12 +1,69 @@
-# React + Vite
+## useState()
+useState is a React Hook that lets you add a state variable to your component.
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+```const [state, setState] = useState(initialState)```
 
-Currently, two official plugins are available:
+### Usage
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- Adding state to a component
+- Updating state based on the previous state
+- Updating objects and arrays in state
+- Avoiding recreating the initial state
+- Resetting state with a key
+- Storing information from previous renders
 
-## Expanding the ESLint configuration
+### Parameters
 
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+#### setState()
+- The set function returned by useState lets you update the state to a different value and trigger a re-render. 
+- You can pass the next state directly, or a function that calculates it from the previous state: ```setState((prevState) => prevState + 1)```
+### Returns 
+useState returns an array with exactly two values:
+
+
+## useCallback()
+useCallback is a React Hook that lets you cache a function definition between re-renders.
+
+```const cachedFn = useCallback(fn, dependencies)```
+### Usage
+- Skipping re-rendering of components
+- Updating state from a memoized callback
+- Preventing an Effect from firing too often
+- Optimizing a custom Hook
+
+### Parameters
+
+#### ```fn```
+The function value that you want to cache. It can take any arguments and return any values. ***React will return (not call!) your function back to you during the initial render***. On next renders, React will give you the same function again if the dependencies have not changed since the last render. Otherwise, it will give you the function that you have passed during the current render, and store it in case it can be reused later. ***React will not call your function. The function is returned to you so you can decide when and whether to call it.***
+
+#### dependencies
+**The list of all reactive values referenced inside of the fn code**. Reactive values include props, state, and all the variables and functions declared directly inside your component body. If your linter is configured for React, it will verify that every reactive value is correctly specified as a dependency. The list of dependencies must have a constant number of items and be written inline like [dep1, dep2, dep3]. React will compare each dependency with its previous value using the **Object.is** comparison algorithm.
+
+## use Effect()
+```useEffect(setup, dependencies?)```
+
+#### setup
+callback function
+
+
+## ***NOTE***
+#### Difference between dependencies of useEffect adn useCallback
+
+- ***useCallback dependencies are used for optimization thus use also pass a setState in which the value is being set for cache optimization***
+
+- ***useEffect dependencies are used for the calling of setup callback function if there is any change in the dependencies***
+
+
+## useRef()
+
+- useRef is a React Hook that lets you reference a value that’s not needed for rendering.
+- Used when you have to take reference of anything 
+- Used to provide good UI by selecting a specific fields that changes on action
+- **It return a ref Object**
+
+```const ref = useRef(initialValue)```
+
+### Usage
+- useRef returns a **ref object** with a single *****current*** property** initially set to the **initial value** you provided.
+
+- Changing a ref does not trigger a re-render. This means refs are perfect for storing information that doesn’t affect the visual output of your component.
--- 08_passwordGenerator/src/App.jsx
+++ 08_passwordGenerator/src/App.jsx
@@ -1,15 +1,138 @@
-import { useState } from 'react'
+import { useState, useCallback, useEffect, useRef } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowrd] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+
+  const [password, setPassword] = useState("");
+
+  // useRef(): useRef is a React Hook that lets you reference a value that’s not needed for rendering.
+  const passwordRef = useRef(null);
+
+
+  // useCallback is a React Hook that lets you cache a function definition between re-renders.
+  // const cachedFn = useCallback(fn, dependencies)
+  // dependecies are on which the function re-renders it self :- passed as an array --> [dep1, dep2, ...]
+  /*
+    Usage
+     - Skipping re-rendering of components
+     - Updating state from a memoized callback
+     - Preventing an Effect from firing too often
+     - Optimizing a custom Hook
+*/
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*()_+=[]{}`~";
+
+    for (let index = 1; index <= length; index++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass)
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+  // const copyPasswordToClipboard = () => {
+  //   window.navigator.clipboard.writeText(password);    // will work no issue but not optimal
+  // }
+
+  const copyPasswordToClipboard = useCallback(()=>{
+    console.log(passwordRef.current)
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0,3);
+    window.navigator.clipboard.writeText(password)
+  },[password])
+
+  // ##NOTE the dependency in useCallback are for Optimization whereas the dependencies in
+  // useEffect are that if the changes the will run the callback in useEffect
+
+  // password or you can also pass setPassword these are for optimization 
+  // as useCallback use the concept of memoization thus give one dependency where the value is 
+  // being set 
+
+  /*
+  useCallback will re function if any value changes in the dependencies
+  React will not call your function. 
+  The function is returned to you so you can decide when and whether to call it.
+
+  passwordGenerato(); // Error you cannot call it as you cannot control the rendering it 
+                      // is controled by react itself by setValue 
+
+  Thus using useEffect we call that function
+
+  useEffect is a React Hook that lets you synchronize a component with an external system.
+  Syntax: useEffect(setup, dependencies?)
+  any changes is dependencies the setup callback will run again.
+  As in our example the passwordGenerator will be called by useEffect() and 
+  setPassword() will render this on screen
+  
+  */
+
+  useEffect(()=>{
+    passwordGenerator()
+  }, [length, numberAllowed, charAllowed, passwordGenerator]) 
 
   return (
     <>
-      <h1>Password generator</h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+        <h1 className="text-white text-center my-3">Password generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="w-full px-4 py-2 border border-rose-50"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button 
+            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+            onClick={copyPasswordToClipboard}
+          >
+            copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              value={length}
+              min={6}
+              max={100}
+              onChange={(e) => setLength(e.target.value)}
+            />
+            <label>Length: {length}</label>
+            <input
+              type="checkbox"
+              value={numberAllowed}
+              onChange={() => {
+                setNumberAllowrd((prev) => !prev);
+              }}
+            />
+            <label>Number</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="characterInput">Characters</label>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
