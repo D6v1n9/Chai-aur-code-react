6de833f5f3aa01b09749e789fc1411d7065236b0
D6v1n9 <karansinghlodhi6194u@gmail.com>
2025-06-01 13:56:20 +0530

    09_currencyConvertor project setup

---
+++ 09_currencyConvertor/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl bg-amber-700 ">Currency convertor with chai</h1>
+    </>
+  )
+}
+
+export default App
